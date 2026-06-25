364237929fd79fa00742206a950233fedc5f65af
Ghostw833 <ghostw833@gmail.com>
2026-06-26 02:30:30 +0300

    HW_6

--- HW_6/C4.c
+++ HW_6/C4.c
@@ -1,32 +1,34 @@
-/*
- * C4.c
- * 
- * Copyright 2026 Санёк <Санёк@DESKTOP-ICTHTT6>
- * 
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- * 
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
- * MA 02110-1301, USA.
- * 
- * 
- */
-
-
 #include <stdio.h>
 
-int main(int argc, char **argv)
+int f (int x)
 {
-	
+
+	if (x >= -2 && x < 2)
+	return x*x;
+	else if (x >= 2)
+	return x*x+4*x+5;
+	else 
+	return 4;
+}
+
+
+
+int main ()
+
+{
+	int x,max_f;
+	int first = 1; 
+	while (scanf("%d", &x) == 1 && x != 0) {
+		int current_f = f(x);
+		if (first) {
+			max_f = current_f;
+			first = 0; 
+		} 
+		else if (current_f > max_f) {
+			max_f = current_f;
+		}
+	} 
+	printf("%d",max_f);
 	return 0;
 }
 
--- HW_6/C5.c
+++ HW_6/C5.c
@@ -1,32 +1,21 @@
-/*
- * C5.c
- * 
- * Copyright 2026 Санёк <Санёк@DESKTOP-ICTHTT6>
- * 
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- * 
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
- * MA 02110-1301, USA.
- * 
- * 
- */
-
-
 #include <stdio.h>
 
-int main(int argc, char **argv)
+int sum (int n)
 {
-	
+	int min = 1;
+	if (n < min) 
 	return 0;
+	else 
+	return n;
 }
 
+int main ()
+{
+	int n, result=0;
+	scanf("%d",&n);
+	for (int i = 1; i <= n; i++) {
+		result = result + i;
+	}
+	printf("%d\n",result);
+	return 0;
+}
